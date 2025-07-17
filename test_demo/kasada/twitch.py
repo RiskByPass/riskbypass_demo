886cc045cc0023f581f2dee4d142145024634b59
RiskByPass <support@riskbypass.com>
2025-07-17 15:15:19 +0800

    twitch update

--- test_demo/kasada/twitch.py
+++ test_demo/kasada/twitch.py
@@ -1,4 +1,3 @@
-import requests
 from curl_cffi import requests as crequests
 import json
 
@@ -14,7 +13,7 @@ def get_ct(site, proxy, api_key):
             'proxy': proxy,
         },
     }
-    response = requests.post('http://spiderapi.zicp.fun/api/task/sync', headers=headers, json=json_data)
+    response = crequests.post('https://riskbypass.com/api/task/sync', headers=headers, json=json_data)
     print(response.json())
     kasada_result = response.json().get('result', {}).get('result')
     return json.loads(kasada_result)
@@ -31,24 +30,34 @@ def get_cd(ct, st, api_key):
             "st": st
         }
     }
-    response = requests.post('http://spiderapi.zicp.fun/api/task/sync', headers=headers, json=json_data)
+    response = crequests.post('https://riskbypass.com/api/task/sync', headers=headers, json=json_data)
     print(response.json())
     cd = response.json().get('result', {}).get('result')
     return cd
 
-proxy = 'http://127.0.0.1:8989' # Your proxy
-api_key = 'Your api key' # Your api key
+proxy = 'http://username:password@ip:port' # Your proxy
+api_key = 'Your API Key' # Your api key
 site = 'passport.twitch.tv' # The kasada protection site domain
 
 kasada_args = get_ct(site=site, proxy=proxy, api_key=api_key)
 ct = kasada_args['x-kpsdk-ct'] # Get the x-kpsdk-ct
 st = kasada_args['x-kpsdk-st'] # Get the x-kpsdk-st
 v = kasada_args['x-kpsdk-v'] # Get the x-kpsdk-v
-cd = get_cd(ct=ct, st=st, api_key=api_key) # Get the x-kpsdk-cd
+cd = get_cd(ct=ct, st=0, api_key=api_key) # Get the x-kpsdk-cd
 user_agent = kasada_args['user-agent'] # Get the user-agent
 sec_ch_ua = kasada_args['sec-ch-ua'] # Get the sec-ch-ua
 sec_ch_ua_platform = kasada_args['sec-ch-ua-platform'] # Get the sec-ch-ua-platform
 
+cookies = {
+    'unique_id': 'ktXuYfdjRxvDpUZ5HBoGmqNtpNfJywXA',
+    'unique_id_durable': 'ktXuYfdjRxvDpUZ5HBoGmqNtpNfJywXA',
+    'twitch.lohp.countryCode': 'SG',
+    'experiment_overrides': '{%22experiments%22:{}%2C%22disabled%22:[]}',
+    'api_token': 'twilight.6201d631f4d083a0a8c8fb5678b599ec',
+    'ga__15_abel-ssn': ct,
+    'ga__15_abel': ct,
+    'server_session_id': '9c98c62199d848efab1967dfba591199',
+}
 
 headers = {
     'Accept': '*/*',
@@ -70,19 +79,7 @@ headers = {
     'x-kpsdk-ct': ct,
     'x-kpsdk-v': v,
 }
-json_data = {
-    "username": "crisitnao",
-    "password": "Z12qwer...",
-    "email": "a123456789@gmail.com",
-    "birthday": {
-        "day": 23,
-        "month": 11,
-        "year": 1998,
-        "isOver18": True
-    },
-    "client_id": "kimne78kx3ncx6brgo4mv6wki5h1ko",
-    "is_password_guide": "nist"
-}
+json_data = {"username":"sadsadghg","password":"Y8Dv*v$C:YyG<aK","email":"a12357mn2@gmail.com","birthday":{"day":12,"month":12,"year":1979,"isOver18":True},"client_id":"kimne78kx3ncx6brgo4mv6wki5h1ko","is_password_guide":"nist"}
 
 for i in range(10):
     print(f'第{i+1}次请求')
@@ -91,7 +88,8 @@ for i in range(10):
             headers=headers,
             json=json_data,
             proxies={'http': proxy, 'https': proxy},
-            impersonate='chrome136'
+            impersonate='chrome136',
+            cookies=cookies
         )
     print(response.text)
     headers['x-kpsdk-cd'] = get_cd(ct=ct, st=st, api_key=api_key)
