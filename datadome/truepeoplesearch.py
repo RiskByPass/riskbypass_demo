a1165d7953e7e83f6124af0ae6e7044da3bfe76c
RiskByPass <support@riskbypass.com>
2026-02-25 21:59:32 +0800

    tps

--- datadome/truepeoplesearch.py
+++ datadome/truepeoplesearch.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 # 由 RiskByPass 面板自动生成
 # 依赖: pip install requests
-import requests, time, json, random, string
+import requests, time, json, random, base64
+from urllib.parse import urlencode
+from json import dumps as json_dumps
 from curl_cffi import requests as c_requests
 
 BASE_URL = "https://riskbypass.com"  # api端口地址
 TOKEN = "your token"  # 访问令牌（作为 x-api-key 发送）
 TIMEOUT = 60  # 超时时间（秒）
-PROXY = f'http://username:password@ip:port'
+PROXY = f'http://username:password@host:port'
 print(PROXY)
 
 def run_task(payload):
@@ -69,9 +71,73 @@ def run_task(payload):
         else:
             print("未知响应：", j)
             return
+        
+     
+def tls_forward(url, method="GET", headers={}, json={}, data='', cookies={}, proxy=None, timeout=30, proxies={}):
+    use_proxy = proxies.get('all') or proxies.get('https') or proxy
+    if json:
+        data = json_dumps(json, separators=(', ', ':'), ensure_ascii=False)
+    if type(data) == str:
+        body_base64 = base64.b64encode(data.encode()).decode()
+    elif type(data) == bytes:
+        body_base64 = base64.b64encode(data).decode()
+    elif type(data) == dict:
+        network_data = urlencode(data)
+        body_base64 = base64.b64encode(network_data.encode()).decode()
+    else:
+        body_base64 = None
+    payload = {
+        "task_type": "tls_forward",
+        "proxy": use_proxy,
+        "url": url,
+        "method": method,
+        "headers": headers,
+        "body_base64": body_base64,
+        "cookies_dict": cookies,
+        "timeout": timeout
+    }
+    if headers.get('user-agent'):
+        payload['user_agent'] = headers.get('user-agent')
+    result = run_task(payload)
+    if not result:
+        raise Exception('TLS Forward Error')
+    else:
+        return RiskbypassResponse(result)
+
+def tls_get(url, headers={}, json={}, data='', cookies={}, proxy=None, timeout=30, proxies={}, params={}):
+    if isinstance(params, dict) and params:
+        urlparams = urlencode(params)
+        url += '?' + urlparams
+    return tls_forward(url, 'GET', headers, json, data, cookies, proxy, timeout, proxies)
+
+
+def tls_post(url, headers={}, json={}, data='', cookies={}, proxy=None, timeout=30, proxies={}, params={}):
+    if isinstance(params, dict) and params:
+        urlparams = urlencode(params)
+        url += '?' + urlparams
+    return tls_forward(url, 'POST', headers, json, data, cookies, proxy, timeout, proxies)
+    
+
+class RiskbypassResponse:
+    def __init__(self, response_dict):
+        # 将字典转换为响应对象
+        self.body = base64.b64decode(response_dict.get('body_base64').encode())
+        self.cookies = response_dict.get('cookies', {})
+        self.elapsed = response_dict.get('elapsed', 0)
+        self.error = response_dict.get('error', None)
+        self.headers = response_dict.get('headers', {})
+        self.ok = response_dict.get('ok', False)
+        self.reason = response_dict.get('reason', '')
+        self.status_code = response_dict.get('status_code', 0)
+        self.text = response_dict.get('text', '')
+        self.url = response_dict.get('url', '')
+    
+    def json(self):
+        return json.loads(self.body)
+
 
 if __name__ == "__main__":
-    crawler_session = c_requests.Session(impersonate='chrome136')
+    crawler_session = c_requests.Session(impersonate='firefox135')
     crawler_session.proxies = {
         'http': PROXY,
         'https': PROXY
@@ -94,26 +160,42 @@ if __name__ == "__main__":
         crawler_session.cookies.set(k, v, domain='.truepeoplesearch.com')
 
     headers = {
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': cf_results.get('ua'),
-        'sec-ch-ua': '"Google Chrome";v="143", "Chromium";v="143", "Not A(Brand";v="24"',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'en',
+        'cache-control': 'no-cache',
+        'pragma': 'no-cache',
+        'priority': 'u=0, i',
+        'referer': 'https://www.truepeoplesearch.com/results?name=235648',
+        'sec-ch-device-memory': '8',
+        'sec-ch-ua': '"Not:A-Brand";v="99", "Google Chrome";v="145", "Chromium";v="145"',
+        'sec-ch-ua-arch': '"x86"',
+        'sec-ch-ua-full-version-list': '"Not:A-Brand";v="99.0.0.0", "Google Chrome";v="145.0.7632.110", "Chromium";v="145.0.7632.110"',
         'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-model': '""',
         'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36',
     }
 
-    response = crawler_session.get('https://www.truepeoplesearch.com/results?name=RHEA+RHINEHART&citystatezip=44060', headers=headers,
-                                proxy=PROXY, impersonate='chrome136')
+    response = tls_get('https://www.truepeoplesearch.com/results?name=RHEA+RHINEHART&citystatezip=44060', headers=headers,
+                                proxy=PROXY, cookies=cookies)
 
     with open('response.html', 'w', encoding='utf8') as f:
         f.write(response.text)
 
 
     print(response.status_code)
-    # print(response.text)
     print(response.url)
+    with open('response.html', 'w', encoding='utf8') as f:
+        f.write(response.text)
 
     
-    response2 = crawler_session.get('https://www.truepeoplesearch.com/find/person/p4ul60n4nur98r62u48n', headers=headers, cookies=cookies)
+    response2 = tls_get('https://www.truepeoplesearch.com/find/person/p4ul60n4nur98r62u48n', headers=headers,
+                                proxy=PROXY, cookies=cookies)
     print(response2.status_code)
-    # print(response.text)
     print(response2.url)
+    with open('response2.html', 'w', encoding='utf8') as f:
+        f.write(response2.text)
