4b8ce9ad5b4b4ee33e984e14a25385d228dacbd6
RiskByPass <support@riskbypass.com>
2026-03-21 06:51:29 -0400

    tls forward demo

---
+++ tls/arrow.py
@@ -0,0 +1,40 @@
+from riskbypass import RiskByPassClient
+
+# -*- coding: utf-8 -*-
+# 由 RiskByPass 面板自动生成
+# 依赖: pip install riskbypass
+import json
+from riskbypass import RiskByPassClient
+
+BASE_URL = "https://riskbypass.com"  # api端口地址
+TOKEN    = "your token"    # 访问令牌（作为 x-api-key 发送）
+TIMEOUT  = 120                         # 任务最大执行时间（秒）, 超出此时间将抛出异常
+PROXY = 'http://xxxxxxxxxx__cr.us:xxxxxxxxxx@gw.dataimpulse.com:823'
+
+client = RiskByPassClient(token=TOKEN)
+
+headers = {
+    ":method": "GET",
+    ":authority": "www.arrow.com",
+    ":path": "/en/products/verdin-i-mx95-evaluation-kit/toradex-ag.html",
+    ":scheme": "https",
+    "pragma": "no-cache",
+    "cache-control": "no-cache",
+    "sec-ch-ua": "\"Chromium\";v=\"146\", \"Not-A.Brand\";v=\"24\", \"Google Chrome\";v=\"146\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "sec-fetch-site": "none",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-user": "?1",
+    "sec-fetch-dest": "document",
+    "accept-encoding": "gzip, deflate, br, zstd",
+    "accept-language": "en",
+    "priority": "u=0, i"
+}
+
+response = client.tls_get('https://www.arrow.com/en/products/verdin-i-mx95-evaluation-kit/toradex-ag.html', headers=headers, proxies={'https':PROXY})
+print(response.text)
+print(response.status_code)
